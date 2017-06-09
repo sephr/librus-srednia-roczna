bdc5acef31121b83f3f622e180bdada8990ec0d8
Szymon Borda <szbk20@gmail.com>
2017-06-09 16:15:18 +0200

    Create librus_srednia.js

--- librus_srednia.js
+++ librus_srednia.js
@@ -1,9 +1,9 @@
-var x, y;
+var x = 0, y = 0;
 $('.stretch a').each(function() {
-    if ($(this).prop('title').includes("Kategoria: roczna")) {
+    if ($(this).prop('title').includes('Kategoria: roczna')) {
         x += parseInt($(this).html());
         y++;
     }
 });
-console.log("Twoja średnia wynosi " + x / y);
-alert("Twoja średnia wynosi " + x / y);
+console.log('Twoja średnia wynosi ' + x / y);
+alert('Twoja średnia wynosi ' + x / y);
